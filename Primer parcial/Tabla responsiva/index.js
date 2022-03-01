0137d06e51296faf070450d3c766e3dd4835df4c
Alan P <alancin7@live.com.mx>
2022-02-28 21:53:30 -0600

    Tabla responsiva API Rick and Morty

---
+++ Primer parcial/Tabla responsiva/index.js
@@ -0,0 +1,63 @@
+(async () => {
+  const api = "https://rickandmortyapi.com/api/character/";
+  await fetch(api)
+    .then((response) => response.json())
+    .then((datos) => {
+      rellenarTablaConDatosAPI(datos);
+    })
+    .catch((error) => {
+      console.log(error);
+      fetch("./data.json")
+        .then((response) => response.json())
+        .then((datos) => {
+          rellenarTablaConDatosAPI(datos);
+        });
+    });
+})();
+
+const traducciones = {
+  name: "Nombre",
+  status: "Estatus",
+  species: "Especie",
+  gender: "Genero",
+  nombreOrigen: "Origen",
+  image: "Imagen",
+};
+
+const rellenarTablaConDatosAPI = ({ results }) => {
+  const tabla = document.querySelector("#main-table tbody");
+  const personajesTabla = results.map(
+    ({
+      name,
+      status,
+      species,
+      gender,
+      origin: { name: nombreOrigen },
+      image,
+    }) => {
+      return {
+        name,
+        status,
+        species,
+        gender,
+        nombreOrigen,
+        image,
+      };
+    }
+  );
+  for (let element of personajesTabla) {
+    let nuevoRow = tabla.insertRow();
+    for (key in element) {
+      let nuevaCelda = nuevoRow.insertCell();
+      let valor;
+      if (key === "image") {
+        valor = document.createElement("img");
+        valor.src = element[key];
+      } else {
+        valor = document.createTextNode(element[key]);
+      }
+      nuevaCelda.setAttribute("data-label", traducciones[key]);
+      nuevaCelda.appendChild(valor);
+    }
+  }
+};
