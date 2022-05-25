3927304e92013e09d3bf71cded3587616f4bfc7f
Alan Peña <alancin7@live.com.mx>
2022-05-25 00:59:49 -0500

    Ruta de borrado

---
+++ Ejercicio final/api/gateway/delete.php
@@ -0,0 +1,25 @@
+<?php
+    header('Content-Type: application/json; charset=utf-8');
+    header("Access-Control-Allow-Origin: *");
+    
+    include './../controllers/programadorController.php';
+    include './../response/response.php';
+
+    $programadorController = new ProgramadorController();
+    $response = new Response();
+
+    //Delete programador by id
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        $programador = $programadorController->delete($id);
+        if($programador){
+            $response->setResponse(200, 'Programador eliminado correctamente', $programador);
+        }else{
+            $response->setResponse(404, 'No se encontró el programador', null);
+        }
+    }else{
+        $response->setResponse(400, 'Se esperaba un id', null);
+    }
+
+    echo $response->toJson();
+?>
