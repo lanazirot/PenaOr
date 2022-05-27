fc239a203b186fe3a524b720be4d09a4b6c64db4
Alan Peña <alancin7@live.com.mx>
2022-05-27 00:26:37 -0500

    fixes: actualizar programador

--- Ejercicio final/js/listado/operaciones.js
+++ Ejercicio final/js/listado/operaciones.js
@@ -26,7 +26,9 @@ const listarProgramadores = async () => {
 }
 
 
-const eliminarProgramador = async id => {
+const eliminarProgramador = async () => {
+    const id = $('#id').val();
+    console.log(id);
     const peticion = await fetch(`../api/gateway/delete.php?id=${id}`, {
         method: 'POST'
     });
@@ -40,7 +42,7 @@ const eliminarProgramador = async id => {
     }
 }
 
-const modificarProgramador = async programador => {
+const modificarProgramador = async () => {
     //Obtener datos del modal y asignarlos al programador
     const entries = Object.fromEntries(new FormData(document.getElementById('programadorForm')));
     const nuevoProgramador = Programador.from(entries);
@@ -48,6 +50,7 @@ const modificarProgramador = async programador => {
     nuevoProgramador.visa_laser = entries.visa_laser === 'on';
     nuevoProgramador.vehiculo_personal = entries.vehiculo_personal === 'on';
     nuevoProgramador.equipo_personal = entries.equipo_personal === 'on';
+
 
     //Enviar los datos al servidor
     const peticion = await fetch(`../api/gateway/update.php`, {
@@ -57,46 +60,47 @@ const modificarProgramador = async programador => {
     const { code, message } = await peticion.json();
     if (code === 200) {
         Swal.fire('Exito!', message, 'success');
-        //Actualizar la fila
-        $('#' + nuevoProgramador.id).replaceWith($('<tr>', {
-            id: nuevoProgramador.id,
-            append: `<td>${nuevoProgramador.id}</td>
-            <td>${nuevoProgramador.nombre}</td>
-            <td>${nuevoProgramador.apellidos}</td>
-            <td>${nuevoProgramador.correo}</td>
-            <td>${nuevoProgramador.departamento}</td>`,
-            on: {
-                dblclick() {
-                    mostrarProgramador(nuevoProgramador);
-                }
-            }
-        }));
+        //Actualizar los campos en la fila
+        $('#' + nuevoProgramador.id).children().eq(1).text(nuevoProgramador.nombre);
+        $('#' + nuevoProgramador.id).children().eq(2).text(nuevoProgramador.apellidos);
+        $('#' + nuevoProgramador.id).children().eq(3).text(nuevoProgramador.correo);
+        $('#' + nuevoProgramador.id).children().eq(4).text(nuevoProgramador.departamento);
     }
     else {
         Swal.fire('Oops!', message, 'error');
     }
-
-
-
 };
 
 const mostrarProgramador = async programador => {
     const programadorModal = new bootstrap.Modal(document.getElementById('programadorModal'), {});
-    $('#nombreCompleto').text(programador.nombre.concat(' ').concat(programador.apellidos));
-    $('#nombre').val(programador.nombre);
-    $('#id').val(programador.id);
-    $('#apellidos').val(programador.apellidos);
-    $('#departamento').val(programador.departamento);
-    $('#detalles').text(programador.detalles);
-    $('#correo').val(programador.correo);
-    $('#pais').val(programador.pais);
-    $('#estado_civil').val(programador.estado_civil);
-    $('#visa_laser').prop('checked', programador.visa_laser);
-    $('#vehiculo_personal').prop('checked', programador.vehiculo_personal);
-    $('#equipo_personal').prop('checked', programador.equipo_personal);
-    $('#eliminarProgramador').click(() => { eliminarProgramador(programador.id); programadorModal.hide(); });
-    $('#modificarProgramador').click(() => { modificarProgramador(programador); programadorModal.hide(); });
-    programadorModal.show();
+    //Peticion fetch para traer todos los datos por el ID del programador
+    const peticion = await fetch(`../api/gateway/getById.php?id=${programador.id}`);
+    const { code, data, message } = await peticion.json();
+
+    //Obtener datos del programador
+    const { id, nombre, apellidos, correo, departamento,
+        visa_laser, vehiculo_personal, equipo_personal,
+        estado_civil, pais, detalles } = data;
+
+    if (code === 200) {
+        $('#id').val(id);
+        $('#nombre').val(nombre);
+        $('#apellidos').val(apellidos);
+        $('#nombreCompleto').text(nombre.concat(' ').concat(apellidos));
+        $('#departamento').val(departamento);
+        $('#detalles').text(detalles);
+        $('#correo').val(correo);
+        $('#pais').val(pais);
+        $('#estado_civil').val(estado_civil);
+        $('#visa_laser').prop('checked', visa_laser);
+        $('#vehiculo_personal').prop('checked', vehiculo_personal);
+        $('#equipo_personal').prop('checked', equipo_personal);
+        $('#eliminarProgramador').click(() => { eliminarProgramador(); programadorModal.hide(); });
+        $('#modificarProgramador').click(() => { modificarProgramador(); programadorModal.hide(); });
+        programadorModal.show();
+    } else {
+        Swal.fire('Oops!', message, 'error');
+    }
 }
 
-export { eliminarProgramador, modificarProgramador, mostrarProgramador, listarProgramadores };
+export { listarProgramadores };
