d36082b6214fb705e1c197a75d157828e2d875c5
Alan Peña <alancin7@live.com.mx>
2022-05-27 00:58:43 -0500

    feature: busqueda de programadores

--- Ejercicio final/js/listado/getAllProgramadores.js
+++ Ejercicio final/js/listado/getAllProgramadores.js
@@ -1,6 +1,15 @@
-import {listarProgramadores} from './operaciones.js';
+import { listarProgramadores } from './operaciones.js';
 
 $(document).ready(async () => {
     //Obtenemos todos los programadores
     listarProgramadores();
+    //SOF -> Filtrar programadores
+    $('#filter').keyup(function () {
+        var rex = new RegExp($(this).val(), 'i');
+        $('.searchable tr').hide();
+        $('.searchable tr').filter(function () {
+            return rex.test($(this).text());
+        }).show();
+
+    })
 });
